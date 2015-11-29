43ee77848c43c28a81034aeabf11614faa345a9d
adam mcbride <adam.mcbride@gmail.com>
2015-11-29 16:46:19 -0500

    starting point
    using some of the collision code as a starting point

---
+++ final/sketch.js
@@ -0,0 +1,23 @@
+var back;
+
+function setup() {
+  createCanvas(600, 750);  // sets up canvas
+  back=0;
+  strokeWeight(2);
+  	rectMode(CENTER);
+}
+
+
+
+
+// function draw() {
+// 	background(back);
+// 	fill(255);
+// 	rect(250, 250, 50, 50);
+// 	rect(mouseX, mouseY, 50, 50);
+// 	if(mouseX>(250-50)& mouseX<(250+50)&mouseY>(250-50)&mouseY<(250+50) ){
+// 		back=150;
+// 	}else{
+// 		back=0;
+// 	}
+// }
